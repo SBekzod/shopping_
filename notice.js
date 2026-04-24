54dfe9dbacd1ccb80e0cd413f5b88b3be14fe3b5
martin <martin@comets.kr>
2026-04-24 15:41:23 +0900

    feat: thinking about fullstack

---
+++ notice.js
@@ -0,0 +1,13 @@
+/*
+
+FullStack = Restaurant
+
+Frontend Development:
+    - BSSR (DTE/JTE/EJS)
+    - SPA (React/Angular/Vue)
+
+API REQUEST:
+    - Design > Traditional API | Rest API | GraphQL API
+    - Method > GET | POST
+    - Structure > header | body
+*/
