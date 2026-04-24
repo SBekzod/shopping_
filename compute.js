3071d47e83455b6affb9bcc5a9fb371ee9707009
martin <martin@comets.kr>
2026-04-24 17:33:09 +0900

    fix: Promise and Callback hell

--- compute.js
+++ compute.js
@@ -13,10 +13,26 @@ function division(a, b, callback) {
 }
 
 // call 
-division(10, 0, function (err, data) {
+division(10, 3, function (err, data) {
     if(err) console.log("ERROR:", err);
     else {
         console.log("RESULT:", data)
         console.log("...");
+
+        division(10, 4, function (err, data) {
+            if(err) console.log("ERROR:", err);
+            else {
+                console.log("RESULT:", data)
+                console.log("...");
+
+                division(20, 7, function (err, data) {
+                    if(err) console.log("ERROR:", err);
+                    else {
+                        console.log("RESULT:", data)
+                        console.log("...");
+                    }
+                });
+            }
+        });
     }
 });
